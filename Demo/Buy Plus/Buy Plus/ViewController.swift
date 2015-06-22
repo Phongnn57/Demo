02aba011fc22f0e97e6febb00378e5179d4c87f2
Nam Phong <phongnguyennam@zamba.vn>
2015-06-22 10:28:36 +0700

    first commit

---
+++ Demo/Buy Plus/Buy Plus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Buy Plus
+//
+//  Created by Nam Phong on 6/22/15.
+//  Copyright (c) 2015 BuyPlus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
